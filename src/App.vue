655dffbed0df08c446582862cb5a60bbabc0e8bc
Hyo Tei <serinuntiusu@yahoo.co.jp>
2024-09-08 12:45:04 +0000

    uuid

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue"
+
+const genUuids = ref([])
+for (let index = 0; index < 10; index++) {
+  genUuids.value[index] = self.crypto.randomUUID()
+}
+const regenerate = ()=>{
+  genUuids.value = []
+  for (let index = 0; index < 10; index++) {
+    genUuids.value[index] = self.crypto.randomUUID()
+  }
+}
 </script>
 
 <template>
   <main>
-    HelloWorld
+    <button @click="regenerate">regenerate</button>
+    <li v-for="(item, index) in genUuids" :key="index">{{ item }}</li>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
